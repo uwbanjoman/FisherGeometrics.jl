e567d8f69c88784665becdff735099231ad6d3b4
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-07-05 13:31:46 +0200

    Add is_density_matrix function to validate density matrices

--- src/DensityMatrices.jl
+++ src/DensityMatrices.jl
@@ -12,3 +12,28 @@ function maximally_mixed(n::Integer)
     return Matrix{Float64}(I, n, n) / n
 
 end
+
+"""
+    is_density_matrix(ρ)
+
+Checks whether ρ is a valid density matrix.
+"""
+function is_density_matrix(ρ::AbstractMatrix; atol=1e-12)
+
+    # square
+    size(ρ,1)==size(ρ,2) || return false
+
+    # Hermitian
+    ishermitian(ρ) || return false
+
+    # trace = 1
+    abs(tr(ρ)-1) < atol || return false
+
+    # positive semidefinite
+    λ = eigvals(Hermitian(ρ))
+
+    minimum(real.(λ)) > -atol || return false
+
+    return true
+
+end
