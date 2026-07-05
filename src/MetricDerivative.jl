99e6cad2cb2711c9e477c2bd2643b111a674d67d
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-07-05 17:04:31 +0200

    Implement metric_derivatives function
    Add function to compute directional derivatives of Fisher metric.

--- src/MetricDerivative.jl
+++ src/MetricDerivative.jl
@@ -40,3 +40,48 @@ function dmetric(::FisherMetric,
     return -real(tr(X * term))/2
 
 end
+
+"""
+    metric_derivatives(g, ρ, basis)
+
+Compute the directional derivatives of the Fisher metric in the
+basis `basis`.
+
+Returns a 3-tensor `dg` with
+
+    dg[i,j,k] = Dgρ(H_k; X_i, X_j),
+
+where
+
+- `X_i = basis[i]`,
+- `H_k = basis[k]`.
+
+The tensor `dg` is symmetric in its first two indices.
+"""
+function metric_derivatives(g::FisherMetric,
+                            ρ::AbstractMatrix,
+                            basis)
+
+    n = length(basis)
+
+    dg = zeros(Float64, n, n, n)
+
+    for i in 1:n
+        Xi = basis[i]
+
+        for j in i:n
+            Xj = basis[j]
+
+            for k in 1:n
+                Hk = basis[k]
+
+                val = dmetric(g, ρ, Xi, Xj, Hk)
+
+                dg[i,j,k] = val
+                dg[j,i,k] = val
+            end
+        end
+    end
+
+    return dg
+end
