f8d2722545b8d282307dea726b3682b121c08152
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-07-10 00:07:45 +0200

    Optimize riemann function for performance
    Refactored the riemann function for optimization, reducing nested loops and improving performance with precomputed tensors.

--- src/Curvature.jl
+++ src/Curvature.jl
@@ -12,112 +12,78 @@ associated with the Fisher metric.
 Returns an n×n×n×n array, where
 n = length(basis).
 """
-function riemann(g::FisherMetric,
-                 ρ::AbstractMatrix,
-                 basis)
-
+function riemann(g::FisherMetric, ρ::AbstractMatrix, basis)
     n = length(basis)
 
     G    = metric_matrix(g, ρ, basis)
     Ginv = pinv(G)
 
-    dg  = metric_derivatives(g, ρ, basis)
-    ddg = ddmetric_tensor(g, ρ, basis)
+    dg   = metric_derivatives(g, ρ, basis)   # n × n × n
+    ddg  = ddmetric_tensor(g, ρ, basis)     # n × n × n × n
+    Γ    = christoffel(g, ρ, basis)          # n × n × n
 
-    Γ = christoffel(g, ρ, basis)
+    # --- OPTIMALISATIE 1: Bereken dGinv vooraf via matrix-multiplicaties ---
+    # dGinv_tensor[m, l, i] = ∂ᵢ G⁻¹_{ml}
+    dGinv_tensor = zeros(Float64, n, n, n)
+    for i in 1:n
+        # Pak de slice van dg in de i-richting: dg[:, :, i]
+        dg_i = @view dg[:, :, i]
+        # De contractie -Ginv * dg_i * Ginv via geoptimaliseerde matrix-multiplicatie
+        dGinv_tensor[:, :, i] .= -Ginv * dg_i * Ginv
+    end
 
-    dΓ = zeros(Float64,n,n,n,n)
+    dΓ = zeros(Float64, n, n, n, n)
 
-    #
-    # dΓ[m,i,j,k]
-    #
-    # = ∂ᵢ Γᵐⱼₖ
-    #
-
-    for m in 1:n
-        for i in 1:n
-            for j in 1:n
-                for k in 1:n
-
+    # --- OPTIMALISATIE 2: Gereduceerde 5D-loop ipv 7D-loop ---
+    for k in 1:n
+        for j in 1:n
+            # Handige shortcuts om herhaaldelijke array-lookups in de loops te vermijden
+            dg_k_j = @view dg[k, :, j]
+            dg_j_k = @view dg[j, :, k]
+            dg_j_k_l = @view dg[j, k, :]
+            
+            for i in 1:n
+                ddg_k_j_i = @view ddg[k, :, j, i]
+                ddg_j_k_i = @view ddg[j, :, k, i]
+                ddg_j_k_l_i = @view ddg[j, k, :, i]
+                
+                for m in 1:n
                     s = 0.0
-
-                    for l in 1:n
-
-                        #
-                        # ∂ᵢ G^{-1}
-                        #
-
-                        dGinv =
-                            0.0
-
-                        for a in 1:n
-                            for b in 1:n
-
-                                dGinv -=
-                                    Ginv[m,a] *
-                                    dg[a,b,i] *
-                                    Ginv[b,l]
-
-                            end
-                        end
-
-                        term1 =
-                            dGinv *
-                            (
-                                dg[k,l,j] +
-                                dg[j,l,k] -
-                                dg[j,k,l]
-                            )
-
-                        term2 =
-                            Ginv[m,l] *
-                            (
-                                ddg[k,l,j,i] +
-                                ddg[j,l,k,i] -
-                                ddg[j,k,l,i]
-                            )
-
-                        s += 0.5*(term1 + term2)
-
+                    @simd for l in 1:n
+                        # Gebruik de vooraf berekende dGinv_tensor (O(1) lookup!)
+                        term1 = dGinv_tensor[m, l, i] * (dg_k_j[l] + dg_j_k[l] - dg_j_k_l[l])
+                        
+                        term2 = Ginv[m, l] * (ddg_k_j_i[l] + ddg_j_k_i[l] - ddg_j_k_l_i[l])
+                        
+                        s += term1 + term2
                     end
-
-                    dΓ[m,i,j,k] = s
-
+                    dΓ[m, i, j, k] = 0.5 * s
                 end
             end
         end
     end
 
-    #
-    # Riemann tensor
-    #
-
-    R = zeros(Float64,n,n,n,n)
-
-    for m in 1:n
-        for i in 1:n
-            for j in 1:n
-                for k in 1:n
-
-                    R[m,i,j,k] =
-                        dΓ[m,i,j,k] -
-                        dΓ[m,j,i,k]
-
-                    for l in 1:n
-
-                        R[m,i,j,k] +=
-                            Γ[m,i,l]*Γ[l,j,k] -
-                            Γ[m,j,l]*Γ[l,i,k]
-
+    # --- Riemann Tensor Assemblage ---
+    R = zeros(Float64, n, n, n, n)
+    for k in 1:n
+        for j in 1:n
+            for i in 1:n
+                for m in 1:n
+                    val = dΓ[m, i, j, k] - dΓ[m, j, i, k]
+                    
+                    # Contractie van de Christoffel-symbolen
+                    s_Γ = 0.0
+                    @simd for l in 1:n
+                        s_Γ += Γ[m, i, l] * Γ[l, j, k] - Γ[m, j, l] * Γ[l, i, k]
                     end
-
+                    
+                    R[m, i, j, k] = val + s_Γ
                 end
             end
         end
     end
 
     return R
-
 end
 
 """
