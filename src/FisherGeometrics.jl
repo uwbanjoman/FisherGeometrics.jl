97672d87ad09e539a7a55fc199e7538afeddcd52
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-07-21 18:22:08 +0200

    Add oneill_correction and bh_partition_sum exports

--- src/FisherGeometrics.jl
+++ src/FisherGeometrics.jl
@@ -91,6 +91,7 @@ export H0, valid_rep
 export spectrum_scalar, spectrum_vector, spectrum_twoform, spectrum_threeform, spectrum_spinor
 export kk_spectrum, massless_check, lowest_massive_spinor
 export  mass_gap_M111, M_min_KK, hierarchy_resolution, seeley_dewitt_analytical
+export oneill_correction, bh_partition_sum
 
 # Operators.jl
 export jordan, Lρ, Rρ, Lρ_matrix, Rρ_matrix
